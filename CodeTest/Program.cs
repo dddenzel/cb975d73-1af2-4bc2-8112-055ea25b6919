d4e56580bb12374056d7971df7ef339e7a4bda84
awoo099 <anthony@wisenet.co>
2024-08-01 15:15:17 +1000

    Missing commit

--- CodeTest/Program.cs
+++ CodeTest/Program.cs
@@ -7,11 +7,31 @@ namespace CodeTest
     {
         static void Main(string[] args)
         {
-            INumberService sequencerService = new NumberSequencerService();
+            // If no arguments or a call for help, show usage
+            if (args.Length == 0 || new[] { "/h", "--h", "/help", "--help" }.Contains(args[0], StringComparer.OrdinalIgnoreCase))
+                ShowUsage();
 
-            var result = sequencerService.FindNumberSequence("6 1 5 9 2");
+            if (args.Length == 1)
+            {
+                INumberService sequencerService = new NumberSequencerService();
+                var result = sequencerService.FindNumberSequence(args[1]);
+                Console.WriteLine(result);
+            }
 
-            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Show usage on console
+        /// </summary>        
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Usage:           CodeTest \"[num1] [num2] ...\"");            
+            Console.WriteLine("Example:         CodeTest \"1 2 45 987 0 67\"");
+            Console.WriteLine("");
+            Console.WriteLine("Input:");
+            Console.WriteLine("[num1] [num2]:   A single or list of numbers seperated by a single space");
+
         }
     }
 }
