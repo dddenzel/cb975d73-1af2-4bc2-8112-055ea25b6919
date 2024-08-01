e21d78a0d1a7063607668a4ac3fbabb758e7d1ed
awoo099 <anthony@wisenet.co>
2024-08-01 12:15:31 +1000

    First cut. Seems to be returning OK data at a glance

---
+++ CodeTestConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using CodeTestConsoleApp.Interfaces;
+using CodeTestConsoleApp.Services;
+
+namespace CodeTestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            INumberService sequencerService = new NumberSequencerService();
+
+            var result = sequencerService.FindNumberSequence("6 1 5 9 2");
+        }
+    }
+}
---
+++ CodeTestConsoleApp/Services/NumberSequencerService.cs
@@ -0,0 +1,40 @@
+﻿using CodeTestConsoleApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTestConsoleApp.Services
+{
+    public class NumberSequencerService : INumberService
+    {
+        public string? FindNumberSequence(string? numberString)
+        {
+            string? result = null;
+            List<List<int>> results = new List<List<int>>();
+
+            var numbers = numberString?.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)?
+                .Select(x => int.Parse(x))?.ToList() ?? new List<int>();
+
+            for (int i = 0; i <= numbers.Count - 1; i++)
+            {
+                var sequentialList = new List<int>();
+                for (int x = i; x <= numbers.Count - 1; x++)
+                {
+                    if (sequentialList.Any() && numbers[x] <= sequentialList.Last())
+                        break;
+
+                    sequentialList.Add(numbers[x]);
+                }
+
+                results.Add(sequentialList);
+            }
+
+            if (results.Any())
+                result = string.Join(" ", results.First(x => x.Count == results.Max(y => y.Count)));
+
+            return result;
+        }
+    }
+}
